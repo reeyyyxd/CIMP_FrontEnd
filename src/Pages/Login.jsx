bd2e900b32c58d892abe3cc5146ec071b82e0843
Zan <sottomezon@gmail.com>
2024-04-17 18:18:57 +0800

    gwapo

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
+import React from 'react';
+
 export default function Login() {
-	return (
-		<h1 className="text-center" style={{ color: "black", fontSize: "24px" }}>
-			Hello World
-		</h1>
-	);
+    return (
+        <h1 className="text-center text-3xl">Welcome to Login Page!</h1>
+    )
 }
