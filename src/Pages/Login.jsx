2c83c228dcc178e5360ba6d988d69b3ca3448c04
Rhyss Gianne M. Almeda <112668254+rhyss17@users.noreply.github.com>
2024-04-17 18:05:56 +0800

    Update login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,8 +13,8 @@ export default function Login() {
                         <input className="w-full p-2 mb-6 text-black border border-black rounded-full outline-none focus:bg-gray-300" type="text" name="username" id="username" />
                     </div>
                     <div>
-                        <label className="block mb-2" htmlFor="password">Password</label>
-                        <input className="w-full p-2 mb-6 text-maroon-700 border border-black rounded-full outline-none focus:bg-gray-300" type="password" name="password" id="password" />
+                        <label className="block mb-2" htmlFor="password">Password</label> 
+                        <input className="w-full p-2 mb-6 border border-black rounded-full outline-none focus:bg-gray-300" type="password" name="password" id="password" />
                     </div>
                     <div>
                     <input className="w-full bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 mb-6 rounded-full" type="submit" value="Login" />
