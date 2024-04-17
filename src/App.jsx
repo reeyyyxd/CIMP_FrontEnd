64885150b454f25abcf7cc4736f814334b01f409
Rey Anthony Novero <2woaranthony@gmail.com>
2024-04-17 18:25:35 +0800

    fix the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
-import Home from "./Pages/Home.d";
+import Home from "./Pages/home";
 
 export default function App() {
   return (
