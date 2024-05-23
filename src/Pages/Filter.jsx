390653d52a14b34a69bf7010a89a2666457a464d
Luquezyyy <143938229+Luquezyyy@users.noreply.github.com>
2024-05-23 22:37:00 +0800

    edited print

--- src/Pages/Filter.jsx
+++ src/Pages/Filter.jsx
@@ -379,15 +379,10 @@ export default function Filter( {user, setUser} ) {
         <table>
             <thead>
                 <tr>
-                    <th>Property Tag</th>
-                    
-                    <th>Invoice Number</th>
-                    <th>Issue Order Number</th>
-                    <th>Accountable Person</th>
-                    <th>Department</th>
-                    <th>Designation</th>
-                    <th>invoiceDate</th>
-                    <th>Serial Number</th>
+                <th>Quantity</th>
+                <th>Unit</th>
+                <th>Description</th>
+                <th>Remarks</th>
                 </tr>
             </thead>
             <tbody>
@@ -395,14 +390,10 @@ export default function Filter( {user, setUser} ) {
     queryResults.forEach(item => {
         printableContent += `
             <tr>
-                <td>${item.iid}</td>
-                <td>${item.invoiceNumber}</td>
-                <td>${item.issueOrder}</td>
-                <td>${item.accPerson}</td>
-                <td>${item.department}</td>
-                <td>${item.designation}</td>
-                <td>${item.invoiceDate}</td>
-                <td>${item.description ? item.description.serialNumber : 'None'}</td>
+                <td>${item.quantity}</td>
+                    <td>${item.unitOfMeasurement}</td>
+                    <td>${item.description ? item.description.name : 'None'}</td>
+                    <td>${item.remarks}</td>
             </tr>
         `;
     });
